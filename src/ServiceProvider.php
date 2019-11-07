b45bbe2aad654a0e7b724174e7ad4cc693b22abc
Panagiotis Messinezis <panagiotis@messinezis.com>
2019-11-07 14:50:50 +0200

    Initial package scaffolding

---
+++ src/ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Theomessin\Argo;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('argo.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'argo');
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Theomessin\Argo\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+
+}
